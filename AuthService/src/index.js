cb37862eccc773e7366405edcb2424a8e6fb3e27
gyanabritish <gyannayak396@gmail.com>
2023-02-07 22:48:20 +0530

    added jwt create and verify logic

--- AuthService/src/index.js
+++ AuthService/src/index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 const UserRepository = require('./repository/user-repository');
+const UserService = require('./services/user-service');
 const prepareAndStartServer = () => {
     
     app.use(bodyParser.json());
@@ -15,9 +16,15 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`server started at: ${PORT}`);
-        const repo = new UserRepository();
-        const response = await repo.getById(1);
-        console.log(response);
+       // const repo = new UserRepository();
+        //const response = await repo.getById(1);
+       // console.log(response);
+       const service = new UserService();
+       const newToken = service.createToken({
+        email: 'sanket@admin.com',
+        id:1
+       });
+       console.log("new token is",newToken);
     });
 }
 
--- AuthService/src/services/user-service.js
+++ AuthService/src/services/user-service.js
@@ -1,5 +1,6 @@
 const UserRepository = require('../repository/user-repository');
-
+const jwt = require('jsonwebtoken');
+const{ JWT_KEY } = require('../config/serverConfig');
 class UserService {
 
     constructor() {
@@ -19,6 +20,26 @@ class UserService {
             throw error;
         }
     }
+
+    createToken(user){
+        try {
+            const result = jwt.sign(user,JWT_KEY,{ expiresIn:'1h'});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in the token creation");
+            throw error;
+        }
+    }
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation", error);
+            throw error;
+        }
+    }
+
 }
 
 module.exports = UserService;
